41979b5c5d77aa9ce1342e91445b31f66824c1db
diegoestrada11 <est21016@byui.edu>
2025-02-08 14:32:40 -0700

    60% now

--- week05/230.05.Ponder/lander.cpp
+++ week05/230.05.Ponder/lander.cpp
@@ -10,13 +10,33 @@
 #include "lander.h"
 #include "acceleration.h"
 
+
  /***************************************************************
   * RESET
   * Reset the lander and its position to start the game over
   ***************************************************************/
 void Lander::reset(const Position& posUpperRight)
 {
-   status = DEAD;
+   // Set the angle of the lander to straight up
+   angle.setUp();
+
+   // Set the status of the lander to PLAYING
+   status = PLAYING;
+
+   // Fill the fuel with 5000.0 units
+   fuel = 5000.0;
+
+   // Set the horizontal velocity to a random value between -4m/s and -10m/s
+   velocity.setDX(random(-10.0, -4.0));
+
+   // Set the vertical velocity to a random value between -2m/s and 2m/s
+   velocity.setDY(random(-2.0, 2.0));
+
+   // Set the horizontal position to 1 pixel from the right of the screen
+   pos.setX(posUpperRight.getX() - 1);
+
+   // Set the vertical position between 75% and 95% of the height of the screen
+   pos.setY(posUpperRight.getY() * random(0.75, 0.95));
 }
 
 /***************************************************************
@@ -25,6 +45,11 @@ void Lander::reset(const Position& posUpperRight)
  ***************************************************************/
 void Lander::draw(const Thrust& thrust, ogstream& gout) const
 {
+   // Draw the lander at its current position and angle
+   gout.drawLander(pos, angle.getDegrees());
+
+   // Turn on the flames as necessary
+   gout.drawLanderFlames(pos, angle.getDegrees(), thrust.isMain(), thrust.isClock(), thrust.isCounter());
 }
 
 /***************************************************************
